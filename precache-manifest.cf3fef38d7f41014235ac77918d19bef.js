c9fb4e7182625e7fefc096dcc916cc74fe304836
Garrett Weinberg <weinberg135@gmail.com>
2020-12-14 19:05:48 -0500

    Updates

--- precache-manifest.cf3fef38d7f41014235ac77918d19bef.js
+++ precache-manifest.cf3fef38d7f41014235ac77918d19bef.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "c1324e1e418151fbd636350626354f26",
+    "revision": "3568137d469693c735e99bd668461c72",
     "url": "/bmlt-api-explorer/index.html"
   },
   {
-    "revision": "7f2a4829d9144e009ef0",
+    "revision": "94716e5a24ba81212a7c",
     "url": "/bmlt-api-explorer/static/css/main.ff8528a3.chunk.css"
   },
   {
-    "revision": "9f7a2feec8abb2d4af18",
-    "url": "/bmlt-api-explorer/static/js/2.84640c2e.chunk.js"
+    "revision": "b35eed90bca75ffa4767",
+    "url": "/bmlt-api-explorer/static/js/2.0b074423.chunk.js"
   },
   {
     "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
-    "url": "/bmlt-api-explorer/static/js/2.84640c2e.chunk.js.LICENSE.txt"
+    "url": "/bmlt-api-explorer/static/js/2.0b074423.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "7f2a4829d9144e009ef0",
-    "url": "/bmlt-api-explorer/static/js/main.48b983f9.chunk.js"
+    "revision": "94716e5a24ba81212a7c",
+    "url": "/bmlt-api-explorer/static/js/main.e9f79b7f.chunk.js"
   },
   {
     "revision": "fa7281f114eea7f9bc0d",
