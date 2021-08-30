c471585d6139fa015aa226f6bfea01ab70b7aeae
Jimmy Neville <jimmyeneville@gmail.com>
2021-08-29 21:18:05 -0400

    Add search type to the Additional Parameters section

--- src/components/query-string/Query.js
+++ src/components/query-string/Query.js
@@ -1,9 +1,8 @@
-import React, {useContext} from 'react'
-import {Querycontext} from '../../context/QueryContext'
-import {Globalcontext} from '../../context/GlobalContext'
+import React, { useContext } from "react";
+import { Querycontext } from "../../context/QueryContext";
+import { Globalcontext } from "../../context/GlobalContext";
 
 function Query() {
-
   const {
     excludedFormats,
     includedFormats,
@@ -16,60 +15,82 @@ function Query() {
     formatComparison,
     htmlSimple,
     textSearch,
+    searchType,
   } = useContext(Querycontext);
 
-  const {root_server_url} = useContext(Globalcontext);
+  const { root_server_url } = useContext(Globalcontext);
 
   //Set Final Formats Array
   let formatsArr = [...includedFormats, ...excludedFormats];
-  if (formatsArr.length > 1 ) {
-    formatsArr = formatsArr.map(i => '&formats[]=' + i);
-    } else {
-    formatsArr = formatsArr.map(i => '&formats=' + i);
+  if (formatsArr.length > 1) {
+    formatsArr = formatsArr.map((i) => "&formats[]=" + i);
+  } else {
+    formatsArr = formatsArr.map((i) => "&formats=" + i);
   }
 
   //Set Final Days Array
   let daysArr = [...includedDays, ...excludedDays];
-  if (daysArr.length > 1 ) {
-    daysArr = daysArr.map(i => '&weekdays[]=' + i);
-    } else {
-    daysArr = daysArr.map(i => '&weekdays=' + i);
+  if (daysArr.length > 1) {
+    daysArr = daysArr.map((i) => "&weekdays[]=" + i);
+  } else {
+    daysArr = daysArr.map((i) => "&weekdays=" + i);
   }
 
   //Set Final Service Bodies Array
   let bodiesArr = [...includedBodies, ...excludedBodies];
-  if (bodiesArr.length > 1 ) {
-    bodiesArr = bodiesArr.map(i => '&services[]=' + i);
-    } else {
-    bodiesArr = bodiesArr.map(i => '&services=' + i);
+  if (bodiesArr.length > 1) {
+    bodiesArr = bodiesArr.map((i) => "&services[]=" + i);
+  } else {
+    bodiesArr = bodiesArr.map((i) => "&services=" + i);
   }
 
-  //Set Text Search 
-  let txtVal = ''
+  //Set Text Search
+  let txtVal = "";
   if (textSearch.length > 0) {
-    txtVal = '&SearchString=' + textSearch;
+    txtVal = "&SearchString=" + textSearch;
   } else {
-    txtVal = '';
+    txtVal = "";
   }
-  
+
+  //Set Search Type
+  let tempSearchString = "";
+  switch (searchType) {
+    case "1":
+      tempSearchString = "&SearchStringAll=1";
+      break;
+    case "2":
+      tempSearchString = "&SearchStringExact=1";
+      break;
+    case "3":
+      tempSearchString = "&StringSearchIsAnAddress=1";
+      break;
+    default:
+      tempSearchString = "";
+  }
+
   //Set Final Query Arrya
-  let joinArr = [...bodiesArr, ...daysArr, ...formatsArr].join('');
+  let joinArr = [...bodiesArr, ...daysArr, ...formatsArr].join("");
+  if (txtVal.length > 0) {
+    txtVal += tempSearchString;
+  }
   let queryArr = joinArr + formatComparison + htmlSimple + txtVal;
-  queryArr = queryArr.replace(/\s/g, '%20');
-  
+  queryArr = queryArr.replace(/\s/g, "%20");
+
   return (
-      <div className="hmmm">
-      {root_server_url !== null ?
-      <>
-      <h5 className="query-heading text-center">Your Query:</h5> 
-      <a className="query-string" href={`${root_server_url}/client_interface/${dataFormat}/${dataQuery}${queryArr}`}>{`${root_server_url}/client_interface/${dataFormat}/${dataQuery}${queryArr}`}</a>
-      </>
-      :
-      <>
-      </>
-    }
+    <div className="hmmm">
+      {root_server_url !== null ? (
+        <>
+          <h5 className="query-heading text-center">Your Query:</h5>
+          <a
+            className="query-string"
+            href={`${root_server_url}/client_interface/${dataFormat}/${dataQuery}${queryArr}`}
+          >{`${root_server_url}/client_interface/${dataFormat}/${dataQuery}${queryArr}`}</a>
+        </>
+      ) : (
+        <></>
+      )}
     </div>
-  )
+  );
 }
 
-export default Query
+export default Query;
--- src/components/text-search/TextSearch.js
+++ src/components/text-search/TextSearch.js
@@ -1,20 +1,42 @@
-import React, {useContext} from 'react'
-import {Querycontext} from '../../context/QueryContext'
+import React, { useContext } from "react";
+import { Querycontext } from "../../context/QueryContext";
 
 function TextSearch() {
-
-  const {textSearchFunction} = useContext(Querycontext)
+  const { textSearchFunction, searchTypeFunction } = useContext(Querycontext);
 
   function setTextSearch(e) {
-    textSearchFunction(e.target.value)
+    textSearchFunction(e.target.value);
+  }
+
+  function setSearchType(e) {
+    searchTypeFunction(e.target.value);
   }
 
   return (
     <div className="form-group">
-    <label htmlFor="textSearch">Text Search</label>
-    <input type="text" className="form-control" id="textSearch" aria-describedby="textSearch" placeholder="Search..." onChange={setTextSearch} />
-  </div>
-  )
+      <label htmlFor="textSearch">Text Search</label>
+      <input
+        type="text"
+        className="form-control"
+        id="textSearch"
+        aria-describedby="textSearch"
+        placeholder="Search..."
+        onChange={setTextSearch}
+      />
+      <select
+        className="form-control custom-select"
+        style={{ marginTop: 10 }}
+        onChange={setSearchType}
+      >
+        <option value={0} default>
+          Do a General "Casual" Text Search
+        </option>
+        <option value={1}>Search for all the words</option>
+        <option value={2}>Search for the Exact Text</option>
+        <option value={3}>This is a Location</option>
+      </select>
+    </div>
+  );
 }
 
-export default TextSearch
+export default TextSearch;
