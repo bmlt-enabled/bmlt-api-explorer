695a8e9ccf4763f3771d20036a0c14956d9d24bd
Jimmy Neville <jimmyeneville@gmail.com>
2021-10-17 22:04:00 -0400

    Add "Meeting Start or End Time" section

--- src/components/query-string/Query.js
+++ src/components/query-string/Query.js
@@ -1,103 +1,104 @@
-import React, { useContext } from "react";
-import { Querycontext } from "../../context/QueryContext";
-import { Globalcontext } from "../../context/GlobalContext";
+import React, { useContext } from 'react'
+import { Querycontext } from '../../context/QueryContext'
+import { Globalcontext } from '../../context/GlobalContext'
 
 function Query() {
-  const {
-    excludedFormats,
-    includedFormats,
-    includedDays,
-    excludedDays,
-    includedBodies,
-    excludedBodies,
-    dataFormat,
-    dataQuery,
-    formatComparison,
-    htmlSimple,
-    textSearch,
-    searchType,
-    searchRadius,
-  } = useContext(Querycontext);
+    const {
+        excludedFormats,
+        includedFormats,
+        includedDays,
+        excludedDays,
+        includedBodies,
+        excludedBodies,
+        dataFormat,
+        dataQuery,
+        formatComparison,
+        htmlSimple,
+        textSearch,
+        searchType,
+        searchRadius,
+        startEndTime,
+    } = useContext(Querycontext)
 
-  const { root_server_url } = useContext(Globalcontext);
+    const { root_server_url } = useContext(Globalcontext)
 
-  //Set Final Formats Array
-  let formatsArr = [...includedFormats, ...excludedFormats];
-  if (formatsArr.length > 1) {
-    formatsArr = formatsArr.map((i) => "&formats[]=" + i);
-  } else {
-    formatsArr = formatsArr.map((i) => "&formats=" + i);
-  }
+    //Set Final Formats Array
+    let formatsArr = [...includedFormats, ...excludedFormats]
+    if (formatsArr.length > 1) {
+        formatsArr = formatsArr.map((i) => '&formats[]=' + i)
+    } else {
+        formatsArr = formatsArr.map((i) => '&formats=' + i)
+    }
 
-  //Set Final Days Array
-  let daysArr = [...includedDays, ...excludedDays];
-  if (daysArr.length > 1) {
-    daysArr = daysArr.map((i) => "&weekdays[]=" + i);
-  } else {
-    daysArr = daysArr.map((i) => "&weekdays=" + i);
-  }
+    //Set Final Days Array
+    let daysArr = [...includedDays, ...excludedDays]
+    if (daysArr.length > 1) {
+        daysArr = daysArr.map((i) => '&weekdays[]=' + i)
+    } else {
+        daysArr = daysArr.map((i) => '&weekdays=' + i)
+    }
 
-  //Set Final Service Bodies Array
-  let bodiesArr = [...includedBodies, ...excludedBodies];
-  if (bodiesArr.length > 1) {
-    bodiesArr = bodiesArr.map((i) => "&services[]=" + i);
-  } else {
-    bodiesArr = bodiesArr.map((i) => "&services=" + i);
-  }
+    //Set Final Service Bodies Array
+    let bodiesArr = [...includedBodies, ...excludedBodies]
+    if (bodiesArr.length > 1) {
+        bodiesArr = bodiesArr.map((i) => '&services[]=' + i)
+    } else {
+        bodiesArr = bodiesArr.map((i) => '&services=' + i)
+    }
 
-  //Set Text Search
-  let txtVal = "";
-  if (textSearch.length > 0) {
-    txtVal = "&SearchString=" + textSearch;
-  } else {
-    txtVal = "";
-  }
+    //Set Text Search
+    let txtVal = ''
+    if (textSearch.length > 0) {
+        txtVal = '&SearchString=' + textSearch
+    } else {
+        txtVal = ''
+    }
 
-  //Set Search Type
-  let tempSearchString = "";
-  switch (searchType) {
-    case "1":
-      tempSearchString = "&SearchStringAll=1";
-      break;
-    case "2":
-      tempSearchString = "&SearchStringExact=1";
-      break;
-    case "3":
-      tempSearchString = "&StringSearchIsAnAddress=1";
-      break;
-    default:
-      tempSearchString = "";
-  }
+    //Set Search Type
+    let tempSearchString = ''
+    switch (searchType) {
+        case '1':
+            tempSearchString = '&SearchStringAll=1'
+            break
+        case '2':
+            tempSearchString = '&SearchStringExact=1'
+            break
+        case '3':
+            tempSearchString = '&StringSearchIsAnAddress=1'
+            break
+        default:
+            tempSearchString = ''
+    }
 
-  //Set Search Radius
-  let tempSearchRadius = "";
-  if (searchType === "3" && searchRadius) {
-    tempSearchRadius = `&SearchStringRadius=${searchRadius}`;
-  }
+    //Set Search Radius
+    let tempSearchRadius = ''
+    if (searchType === '3' && searchRadius) {
+        tempSearchRadius = `&SearchStringRadius=${searchRadius}`
+    }
 
-  //Set Final Query Array
-  let joinArr = [...bodiesArr, ...daysArr, ...formatsArr].join("");
-  if (txtVal.length > 0) {
-    txtVal += tempSearchString + tempSearchRadius;
-  }
-  let queryArr = joinArr + formatComparison + htmlSimple + txtVal;
-  queryArr = queryArr.replace(/\s/g, "%20");
+    //Set Final Query Array
+    let joinArr = [...bodiesArr, ...daysArr, ...formatsArr].join('')
+    if (txtVal.length > 0) {
+        txtVal += tempSearchString + tempSearchRadius
+    }
+    let queryArr = joinArr + formatComparison + htmlSimple + txtVal
+    queryArr = queryArr.replace(/\s/g, '%20')
 
-  return (
-    <div className="hmmm">
-      {root_server_url !== null ? (
-        <>
-          <h5 className="query-heading text-center">Your Query:</h5>
-          <a
-            className="query-string"
-            href={`${root_server_url}/client_interface/${dataFormat}/${dataQuery}${queryArr}`}
-          >{`${root_server_url}/client_interface/${dataFormat}/${dataQuery}${queryArr}`}</a>
-        </>
-      ) : (
-        <></>
-      )}
-    </div>
-  );
+    return (
+        <div className="hmmm">
+            {root_server_url !== null ? (
+                <>
+                    <h5 className="query-heading text-center">Your Query:</h5>
+                    <a
+                        className="query-string"
+                        href={`${root_server_url}/client_interface/${dataFormat}/${dataQuery}${queryArr}${startEndTime}`}
+                    >{`${root_server_url}/client_interface/${dataFormat}/${dataQuery}${queryArr}${startEndTime}`}</a>
+                </>
+            ) : (
+                <></>
+            )}
+        </div>
+    )
 }
 
-export default Query;
+export default Query
